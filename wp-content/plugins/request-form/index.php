6c03f87a1466e39468c11c3d58155e71aff49f4d
Eliza Jeyakumar <Ejeyakumar7206@conestogac.on.ca>
2016-12-09 16:57:08 -0500

    removed line 8-10

--- wp-content/plugins/request-form/index.php
+++ wp-content/plugins/request-form/index.php
@@ -5,9 +5,6 @@ Plugin Name: Example Contact Form Plugin
 
 function html_form_code() {
 	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
-	echo '<p>';
-    echo 'All fields are required </br>';
-    echo '</p>';
     echo '<p>';
 	echo 'Name <br/>';
 	echo '<input type="text" name="cf-name" pattern=" placeholder="required" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
