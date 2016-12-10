9ba45fa95b287b9513ed48ce9df60e3d5f3cebd0
Eliza Jeyakumar <Ejeyakumar7206@conestogac.on.ca>
2016-12-09 19:08:45 -0500

    fix error

--- wp-content/plugins/request-form/index.php
+++ wp-content/plugins/request-form/index.php
@@ -74,13 +74,10 @@ function deliver_mail() {
 		$headers = "From: $name <$email>" . "\r\n";
 
 		// If email has been process for sending, display a success message
-		if ( wp_mail( $to, $message, $headers) ) {
+		 wp_mail( $to, $message, $headers)  {
 			echo '<div>';
 			echo '<p>Thanks for contacting me, expect a response soon.</p>';
 			echo '</div>';
-		} else {
-			echo 'An unexpected error occurred';
-		}
 	}
 }
 
